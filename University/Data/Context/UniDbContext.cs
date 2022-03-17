243daf9725aaf4a4a3518b70a9a7117e0427d5b8
Amir Nematpour <a.nematpour.79@gmail.com>
2022-03-17 21:45:39 +0330

    First Setting and Db Context

---
+++ University/Data/Context/UniDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace University.Data.Context
+{
+    public class UniDbContext : DbContext
+    {
+        public UniDbContext(DbContextOptions<UniDbContext> options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+        }
+    }
+}
--- University/Startup.cs
+++ University/Startup.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -11,6 +12,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using University.Data.Context;
 
 namespace University
 {
@@ -32,11 +34,14 @@ namespace University
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "University", Version = "v1" });
             });
+
+            services.AddDbContext<UniDbContext>(option => option.UseSqlServer(@"Data Source = .; Initial Catalog = UniDb; Integrated Security = True;"));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, UniDbContext uniDbContext)
         {
+            uniDbContext.Database.EnsureCreated();
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
